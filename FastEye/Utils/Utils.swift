346c93dfeaab3cc2051c39c22b84a23c4fbdf8dc
Nicholas <nicholas.allio@me.com>
2017-09-25 23:36:08 +0200

    moved resources, created endviewcontroller and Utils with extensions for Double and UIColor and deleted unused files

---
+++ FastEye/Utils/Utils.swift
@@ -0,0 +1,13 @@
+//
+//  Utils.swift
+//  FastEye
+//
+//  Created by Nicholas Allio on 25/09/2017.
+//  Copyright © 2017 Nicholas Allio. All rights reserved.
+//
+
+import UIKit
+
+class Utils: NSObject {
+
+}
