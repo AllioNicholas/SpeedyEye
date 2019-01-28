fea16d976415ad24c18d3e277491e452a19dae86
Nicholas <nicholas.allio@me.com>
2019-01-28 20:14:49 +0100

    Created initializer with setup parametrs. Also empty function to handle number pressed

--- FastEye/Controllers/Manager/GameManager.swift
+++ FastEye/Controllers/Manager/GameManager.swift
@@ -8,20 +8,68 @@
 
 import UIKit
 
-enum GameMode : String {
+enum GameMode : Int {
     case UpCount
     case DownCount
     case Random
-    
-    // MARK: - Initializers
-    
-    init?(fullType: String) {
-        guard let last = fullType.components(separatedBy: ".").last else { return nil }
-        
-        self.init(rawValue: last)
-    }
 }
 
+private let UpCountModeColor = UIColor(netHex: 0x332433) //Viola
+private let DownCountModeColor = UIColor(netHex: 0x4A5B82) //Azzurro scuro
+private let RandomModeColor = UIColor(netHex: 0x6FA79A) //Verde acqua
+
+let GameInitialValue = "kGameInitialValue"
+let GameColor = "kGameColor"
+
 class GameManager: NSObject {
+    
+    var currentGameMode: GameMode
+    var valueToBeSelected: Int
+    
+    init(gameMode: GameMode, initialSetup: @escaping ([String: Any])->()) {
+        currentGameMode = gameMode
+        
+        var returningSetup: [String:Any]
+        switch self.currentGameMode {
+        case .UpCount:
+            self.valueToBeSelected = 1
+            returningSetup = [
+                GameInitialValue: 1,
+                GameColor: UpCountModeColor
+            ]
+            break
+        case .DownCount:
+            self.valueToBeSelected = 25
+            returningSetup = [
+                GameInitialValue: 25,
+                GameColor: DownCountModeColor
+            ]
+            break
+        case .Random:
+            var num = Int(arc4random_uniform(26))
+            while num == 0 {
+                num = Int(arc4random_uniform(26))
+            }
+            self.valueToBeSelected = Int(num)
+            returningSetup = [
+                GameInitialValue: Int(num),
+                GameColor: RandomModeColor
+            ]
+            break
+        }
+        
+        initialSetup(returningSetup)
+    }
+    
+    func didSelectValue(value: Int, withResultBlock: @escaping (Bool, Int)->()) {
+        switch self.currentGameMode {
+        case .UpCount:
+            break
+        case .DownCount:
+            break
+        case .Random:
+            break
+        }
+    }
     
 }
