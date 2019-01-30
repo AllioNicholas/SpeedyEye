3c241c4e4f9fe7b728fe9fb18f8946247c46f465
Nicholas <nicholas.allio@me.com>
2019-01-30 22:58:07 +0100

    Created TimeManager file

---
+++ FastEye/Controllers/Manager/TimeManager.swift
@@ -0,0 +1,13 @@
+//
+//  TimeManager.swift
+//  FastEye
+//
+//  Created by Nicholas Allio on 30/01/2019.
+//  Copyright © 2019 Nicholas Allio. All rights reserved.
+//
+
+import UIKit
+
+class TimeManager: NSObject {
+
+}
