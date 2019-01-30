10a53a0f08c26416edf4cc0371e55c201cb803a3
Nicholas <nicholas.allio@me.com>
2019-01-30 22:58:54 +0100

    Implemented TimeManager with initializer and endGame function

--- FastEye/Controllers/Manager/TimeManager.swift
+++ FastEye/Controllers/Manager/TimeManager.swift
@@ -9,5 +9,29 @@
 import UIKit
 
 class TimeManager: NSObject {
+    
+    private var gameTimer: Timer = Timer()
+    private var elapsedTime = 0.0
 
+    override init() {
+        super.init()
+    }
+    
+    convenience init(_ updateBlock: @escaping (String)->() ) {
+        self.init()
+        self.gameTimer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { (timer) in
+            self.elapsedTime += 0.01
+            if self.elapsedTime >= 60.0 {
+                let minutes = Int(self.elapsedTime / 60)
+                let secAndMill = self.elapsedTime - Double(60 * minutes)
+                updateBlock(NSString(format: "%d:%05.2f", minutes, secAndMill) as String)
+            } else {
+                updateBlock(NSString(format: "%.2f", self.elapsedTime) as String)
+            }
+        })
+    }
+    
+    func endGame() {
+        self.gameTimer.invalidate()
+    }
 }
